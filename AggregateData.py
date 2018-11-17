3be2613b655b48e9fa8181d9448c038a38953405
Alex Caico <acaico@andrew.cmu.edu>
2018-11-17 17:48:46 -0500

    update

--- AggregateData.py
+++ AggregateData.py
@@ -7,10 +7,6 @@ Created on Sat Nov 17 00:42:46 2018
 import pandas as pd
 import YelpDataAPI as yd
 
-def main():
-    getYelpData()
-    
-    
 
 def getYelpData():
     df = yd.getData()
@@ -21,4 +17,14 @@ def getYelpData():
     
     print('Printing summary data')
     print(df_summary)
+    
+
+def printDataFrametoCSV(df):
+    df.to_csv('yelpData.csv')
+    
+
+def main():
+    getYelpData()
+    
+    
     
