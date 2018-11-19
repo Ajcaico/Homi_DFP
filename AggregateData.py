34c411b3906fdc552055d84029f498ca90ada9fe
Colton Mouritsen <cmourits@andrew.cmu.edu>
2018-11-19 14:07:17 -0500

    Craigslist raw and aggregate data update
    Also updated project prototype excel sheet and AggregateData.py file.

--- AggregateData.py
+++ AggregateData.py
@@ -6,24 +6,25 @@ Created on Sat Nov 17 00:42:46 2018
 """
 ##import each .py file
 import YelpDataAPI as yd
-
+import CraigslistCode as craig
 
 
 
 def getData():
     
 #Call functions from each .py file to get data
-    df_yelp = yd.getData() #returns all yelp data, 1 row for each business
-    df_yelpSummary = yd.getSummaryData(df_yelp) #returns summary with 1 row for each zip and category
-    df_yelpOverallRating = yd.calculateRating(df_yelp, df_yelpSummary) #returns 1 row per zip with overall ratings
+    #df_yelp = yd.getData() #returns all yelp data, 1 row for each business
+    #df_yelpSummary = yd.getSummaryData(df_yelp) #returns summary with 1 row for each zip and category
+    #df_yelpOverallRating = yd.calculateRating(df_yelp, df_yelpSummary) #returns 1 row per zip with overall ratings
+    df_craigslistSummary = craig.getData()
 
 
 
 
 #Print to CSV
-    df_yelp.to_csv('AllYelpData.csv')
-    df_yelpSummary.to_csv('SummaryYelpData.csv')
-    df_yelpOverallRating.to_csv('YelpOverallRating.csv')
+    #df_yelp.to_csv('AllYelpData.csv')
+    #df_yelpSummary.to_csv('SummaryYelpData.csv')
+    #df_yelpOverallRating.to_csv('YelpOverallRating.csv')
     
 
 
