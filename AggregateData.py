d04b8d0792a04957528f5d6273a5688231727a1a
Alex Caico <acaico@andrew.cmu.edu>
2018-11-27 23:19:09 -0500

    fixed yelp df issue

--- AggregateData.py
+++ AggregateData.py
@@ -8,20 +8,9 @@ import ArrestData as arrests
 def getData(): 
 #Call functions from each .py file to get data
     
-    # used to get initial yelp dataset, after which it is saved to a file and pulled from there
-#    df_yelp = yd.getData() #returns all yelp data, 1 row for each business
-#    df_yelpSummary = yd.getSummaryData(df_yelp) #returns summary with 1 row for each zip and category
-#    df_yelpOverallRating = yd.calculateRating(df_yelp, df_yelpSummary) #returns 1 row per zip with overall ratings
-#    df_yelpSummaryTop = df_yelpSummary[df_yelpSummary.category == 'All Restaurants'] # returns 1 row for zipcode for all records cumulative
-#    df_yelpSummaryTop = df_yelpSummaryTop.set_index('zipcode')
-#    df_yelpSummaryTop.to_excel('df_yelpSummaryTop.xlsx')
-
-
     
     #DataFrames to use for aggregation
-    df_yelpSummaryTop = pd.read_excel('df_yelpSummaryTop.xlsx')
-    df_yelpSummaryTop = df_yelpSummaryTop.set_index('zipcode')
-    
+    df_yelpSummaryTop = yd.getData()
     df_craigslistSummary = craig.getData()
     df_zillowSummary = zillow.zillowData()
     df_arrests = arrests.arrestData()
@@ -31,8 +20,7 @@ def getData():
     print("craigslist df size: ", df_craigslistSummary.shape, "\n")
     print("Yelp df size: ", df_yelpSummaryTop.shape, "\n")
     print("Arrests df size: ", df_arrests.shape, "\n")
-    print()
-        
+            
     result = pd.concat([df_zillowSummary, df_craigslistSummary, df_yelpSummaryTop, df_arrests], axis=1, join='outer')
     result.to_excel('Result.xlsx')
 
